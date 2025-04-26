b107d6ba9b7c32330bd56b570479056491fa4240
Can Yarar <cy-genel@hotmail.com>
2025-04-27 01:09:23 +0300

    Created DB models and removed template leftovers.

---
+++ API/Models/Author.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.Models
+{
+    public class Author
+    {
+        // Primary Key.
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public required string Name { get; set; }
+
+        [Required]
+        public DateTime BirthDate { get; set; } 
+
+
+        /*
+        Author: 
+        Id(int, primary key)
+        Name(string, required, max length 100)
+        BArthDate(DateTime, requAred)
+        */
+    }
+}
---
+++ API/Models/Book.cs
@@ -0,0 +1,35 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace API.Models
+{
+    public class Book
+    {
+        // Primary Key.
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(200)]
+        public required string Title { get; set; }
+
+        [Required]
+        public int PublishedYear { get; set; }
+
+        // Foreign key from Author.
+        [Required]
+        public int AuthorId { get; set; }
+
+        [Required]
+        public decimal Price { get; set; }
+        /*
+        Book: 
+        Id(int, primary key)
+        Title(string, required, max length 200)
+        PublishedYear(int, required)
+        AuthorId(foreign key from Author)
+        Price(decimal, required)
+        */
+    }
+}
